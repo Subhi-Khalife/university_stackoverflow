bbcf3959d0424d6d8a4ac9eabd822220d9eaa309
MohammedAttar <mohammedattar997@gmail.com>
2021-03-20 19:55:48 +0200

    Done with the university page.

--- lib/features/sign_up/presentation/pages/sign_up_screen.dart
+++ lib/features/sign_up/presentation/pages/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:university/core/widget/university_drop_down.dart';
 import 'package:university/features/university_with_collage/presentation/bloc/bloc/university_bloc.dart';
 import '../../../../core/widget/app_button.dart';
 import '../../../../core/widget/colors.dart';
@@ -51,6 +52,7 @@ class _SignUpScreen extends State<SignUpScreen> {
   void dispose() {
     super.dispose();
     signUpBloc.close();
+    universityBloc.close();
     _firstNameController.dispose();
     _secondNameController.dispose();
     _emailNameController.dispose();
@@ -65,113 +67,121 @@ class _SignUpScreen extends State<SignUpScreen> {
     ConfigScreen configScreen = ConfigScreen(context);
     WidgetSize widgetSize = WidgetSize(configScreen);
     return Scaffold(
-      body: BlocProvider(
-        create: (context) => signUpBloc,
-        child: BlocBuilder<SignUpBloc, SignUpState>(
-          builder: (context, state) {
-            if (state is SuccessSignUp) {
-              print("Done");
-              return Container(
-                color: Colors.green,
-              );
-            } else if (state is LoadingState) {
-              return Center(
-                child: CircularProgressIndicator(
-                  backgroundColor: colorThemApp,
-                ),
-              );
-            } else {
-              return ListView(
-                padding: EdgeInsets.only(top: 30, left: 12, right: 12),
-                children: [
-                  Text(
-                    "New Account",
-                    style: boldStyle(
-                        color: colorThemApp, fontSize: Constant.xlargeFont),
+      body: ListView(
+        padding: EdgeInsets.only(top: 30, left: 12, right: 12),
+        children: [
+          Text(
+            "New Account",
+            style:
+            boldStyle(color: colorThemApp, fontSize: Constant.xlargeFont),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Welcome to our community in order to sign up, please fill out all filed below",
+            style:
+            regularStyle(color: secondColor, fontSize: Constant.mediumFont),
+          ),
+          SizedBox(height: 10),
+          firstNameController(),
+          SizedBox(height: 10),
+          secondNameController(),
+          SizedBox(height: 10),
+          emailController(),
+          SizedBox(height: 10),
+          mobilePhoneController(),
+          SizedBox(height: 10),
+          dropDownCollage(),
+          SizedBox(height: 10),
+          passwordController(),
+          SizedBox(height: 10),
+          confirmPasswordController(),
+          SizedBox(height: 10),
+          BlocBuilder<SignUpBloc, SignUpState>(
+            builder: (context, state) {
+              if (state is SuccessSignUp) {
+                print("Done");
+                return Container(
+                  color: Colors.green,
+                );
+              } else if (state is LoadingState) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: colorThemApp,
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Welcome to our community in order to sign up, please fill out all filed below",
-                    style: regularStyle(
-                        color: secondColor, fontSize: Constant.mediumFont),
-                  ),
-                  SizedBox(height: 10),
-                  firstNameController(),
-                  SizedBox(height: 10),
-                  secondNameController(),
-                  SizedBox(height: 10),
-                  emailController(),
-                  SizedBox(height: 10),
-                  mobilePhoneController(),
-                  SizedBox(height: 10),
-                  dropDownCollage(),
-                  SizedBox(height: 10),
-                  passwordController(),
-                  SizedBox(height: 10),
-                  confirmPasswordController(),
-                  SizedBox(height: 10),
-                  AppButton(
-                    function: () {
-                      BlocProvider.of<SignUpBloc>(context)
-                        ..add(
-                          SendSignUpRequestEvent(
-                            collageNumber: _collageNumberContoller.text,
-                            email: _emailNameController.text,
-                            lastName: _secondNameController.text,
-                            firstName: _firstNameController.text,
-                            mobile: _phoneNumberController.text,
-                            password: _passwordNameController.text,
-                            collegeId: 6,
-                            universityId: 6,
-                          ),
-                        );
-                    },
-                    name: "Sign up",
-                  ),
-                ],
-              );
-            }
-          },
-        ),
+                );
+              } else {
+                return AppButton(
+                  function: () {
+                    BlocProvider.of<SignUpBloc>(context)
+                      ..add(
+                        SendSignUpRequestEvent(
+                          collageNumber: _collageNumberContoller.text,
+                          email: _emailNameController.text,
+                          lastName: _secondNameController.text,
+                          firstName: _firstNameController.text,
+                          mobile: _phoneNumberController.text,
+                          password: _passwordNameController.text,
+                          collegeId: 6,
+                          universityId: 6,
+                        ),
+                      );
+                  },
+                  name: "Sign up",
+                );
+              }
+            },
+          ),
+        ],
       ),
     );
   }
+
+  //  BlocProvider(
+  //   create: (context) => signUpBloc,
+  //   child:
+
+  //   BlocBuilder<SignUpBloc, SignUpState>(
+  //     builder: (context, state) {
+  //       if (state is SuccessSignUp) {
+  //         print("Done");
+  //         return Container(
+  //           color: Colors.green,
+  //         );
+  //       } else if (state is LoadingState) {
+  //         return Center(
+  //           child: CircularProgressIndicator(
+  //             backgroundColor: colorThemApp,
+  //           ),
+  //         );
+  //       } else {
+  //         return
+  //       }
+  //     },
+  //   ),
+  // ),
 
   Widget dropDownCollage() {
     return
 //      BlocProvider(
 //      (context) => universityBloc,
 //      child:
-        BlocBuilder<UniversityBloc, UniversityState>(builder: (context, state) {
-      if (state is UniversityIsLoadedState) {
-        print("Done");
-        return DropdownButtonHideUnderline(
-          child: DropdownButton<String>(
-            hint: Text(
-              "الجامعة",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+      BlocBuilder<UniversityBloc, UniversityState>(
+        builder: (context, state) {
+          if (state is UniversityIsLoadedState) {
+            print("Done");
+            return UniversityDropDown(
+              elevation: 5,
+              icon: Icon(
+                Icons.arrow_downward,
+                color: Colors.grey,
             ),
-            isExpanded: true,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-            onChanged: (String newValue) {
+              hintText: "الجامعة",
+              valueChanged: (String newValue) {
               setState(() {
                 _selectedUniversity = newValue;
                 print("the select is $_selectedUniversity");
               });
             },
-            value: _selectedUniversity,
-            icon: Icon(
-              Icons.arrow_downward,
-              color: Colors.grey,
-            ),
             items: state.university.data.map(
               (e) {
                 return DropdownMenuItem<String>(
@@ -187,29 +197,76 @@ class _SignUpScreen extends State<SignUpScreen> {
                 );
               },
             ).toList(),
+//          value: _selectedUniversity,
             dropdownColor: Colors.red,
-            elevation: 5,
-          ),
-        );
-      } else if (state is UniversityIsLoadingState) {
-        return Center(
-            child: CircularProgressIndicator(
-          backgroundColor: colorThemApp,
-        ));
-      } else if (state is UniversityIsLoadErrorState) {
-        return Container(
-          color: Colors.red,
-          width: 50,
-          height: 50,
-        );
-      } else {
-        return Container(
-          color: Colors.orange,
-          width: 50,
-          height: 50,
-        );
-      }
-    });
+              isExpandedDropDown: true,
+            );
+//        return DropdownButtonHideUnderline(
+//          child: DropdownButton<String>(
+//            hint: Text(
+//              "الجامعة",
+//              style: TextStyle(
+//                color: Colors.black,
+//                fontSize: 20,
+//                fontWeight: FontWeight.bold,
+//              ),
+//            ),
+//            isExpanded: true,
+//            style: TextStyle(
+//              color: Colors.black,
+//              fontSize: 20,
+//              fontWeight: FontWeight.bold,
+//            ),
+//            onChanged: (String newValue) {
+//              setState(() {
+//                _selectedUniversity = newValue;
+//                print("the select is $_selectedUniversity");
+//              });
+//            },
+//            value: _selectedUniversity,
+//            icon: Icon(
+//              Icons.arrow_downward,
+//              color: Colors.grey,
+//            ),
+//            items: state.university.data.map(
+//              (e) {
+//                return DropdownMenuItem<String>(
+//                  child: Text(e.name),
+//                  value: e.id.toString(),
+//                  onTap: () {
+//                    setState(() {
+//                      _selectedCollage = null;
+//                      _universityIndex = e.id;
+//                      universityName = e.name;
+//                    });
+//                  },
+//                );
+//              },
+//            ).toList(),
+//            dropdownColor: Colors.red,
+//            elevation: 5,
+//          ),
+//        );
+          } else if (state is UniversityIsLoadingState) {
+            return Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: colorThemApp,
+                ));
+          } else if (state is UniversityIsLoadErrorState) {
+            return Container(
+              color: Colors.red,
+              width: 50,
+              height: 50,
+            );
+          } else {
+            return Container(
+              color: Colors.orange,
+              width: 50,
+              height: 50,
+            );
+          }
+        },
+      );
 //    );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:university/features/sign_up/presentation/bloc/sign_up/sign_up_bloc.dart';
 
 import 'features/sign_up/presentation/pages/sign_up_screen.dart';
 import 'features/university_with_collage/presentation/bloc/bloc/university_bloc.dart';
 
 void main() {
   runApp(
-    BlocProvider<UniversityBloc>(
-      create: (context) {
-        return UniversityBloc()..add(FetchUiversity());
-      },
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<UniversityBloc>(
+          create: (context) {
+            return UniversityBloc()
+              ..add(FetchUiversity());
+          },
+        ),
+        BlocProvider<SignUpBloc>(
+          create: (context) {
+            return SignUpBloc()
+              ..add(SendSignUpRequestEvent());
+          },
+        )
+      ],
       child: MyApp(),
     ),
   );
